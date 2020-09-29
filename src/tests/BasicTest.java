aaf4d6f08e085713bcc3426ab8687f2becd42352
Jovana Randelovic <jovana.r2011@gmail.com>
2020-09-29 18:58:26 +0200

    Basic test updated with the common methods among inherreted classes.

--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -1,5 +1,65 @@
 package tests;
 
-public class BasicTest {
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+public class BasicTest {
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+	protected String baseURL = "http://demo.yo-meals.com/";
+	protected String email = "customer@dummyid.com";
+	protected String password = "12345678a";
+
+	@BeforeClass
+
+	public void beforeClass() {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+
+		this.driver = new ChromeDriver();
+		new WebDriverWait(driver, 30);
+		this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		this.driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+	
+	}
+
+	@AfterClass
+
+	public void afterClass() {
+		this.driver.quit();
+	}
+
+	@AfterMethod
+
+	public void afterMethod(ITestResult result) throws IOException {
+
+		if (result.getStatus() == ITestResult.FAILURE) {
+			TakesScreenshot scrShot = ((TakesScreenshot) driver);
+			File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+			String fileName = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss'.png'").format(new Date());
+
+			File DestFile = new File("/screenshots" + fileName);
+			FileHandler.copy(SrcFile, DestFile);
+		} else {
+			System.out.println("Success!");
+		}
+		
+		driver.manage().deleteAllCookies();
+
+		
+	}
 }
+
