a238edc4cf487fdde592d52dd5214be756741c1d
Jovana Randelovic <jovana.r2011@gmail.com>
2020-10-01 21:29:24 +0200

    Notification System page: waiter fixed and code formatted.

--- src/pages/NotificationSystemPage.java
+++ src/pages/NotificationSystemPage.java
@@ -12,29 +12,21 @@ public class NotificationSystemPage extends BasicPage {
 	public NotificationSystemPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
 		super(driver, wait, js);
 	}
-	
-	
-	public WebElement getNotification() {
-		return this.driver.findElement(By.xpath("//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]\r\n" + 
-				""));
-	}
-	
-	public String getNotificationText() {
-		return this.getNotification().getText();
-		
-	}
-//This method does not work	
-	public void waitForNotDisappearance() {
-		WebElement message = this.driver.findElement(By.xpath("//*[contains(@class, 'system_message')]"));
-//	WebElement message = this.driver.findElement(By.xpath("//*[contains(@class, \"system_message\")]"));
 
-	//	wait.until(ExpectedConditions.attributeContains(message, "style", "display: none;"));
-		
-		wait.until(ExpectedConditions.attributeContains(By.xpath("//*[contains(@class, 'system_message')]"), "style", "display: none;"));
-//					<div class="system_message alert alert--success" hidden="hidden" style="display: none;">
-//		<a class="closeMsg" href="javascript:void(0)"></a>
-//		<div class="content">
-//			<div class="div_msg"><ul><li>Logout Successfull!</li></ul></div>	</div>
-//	</div>
+	public WebElement getNot() {
+		return this.driver.findElement(By.xpath(
+				"//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]"));
+	}
+
+	public String getNotText() {
+		return this.getNot().getText();
+
+	}
+
+	public void waitForNotDisappearance() {
+
+		wait.until(ExpectedConditions.attributeContains(By.xpath("//*[contains(@class, 'system_message')]"), "style",
+				"display: none;"));
+
 	}
 }
