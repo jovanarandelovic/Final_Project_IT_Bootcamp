491ccea7ad219e6854cc0edeb7c1c6869c3fa4ab
Jovana Randelovic <jovana.r2011@gmail.com>
2020-10-02 04:27:26 +0200

    Meal Page and Meal Item Test completed. Code formatted.

--- src/pages/MealPage.java
+++ src/pages/MealPage.java
@@ -1,5 +1,53 @@
 package pages;
 
-public class MealPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class MealPage extends BasicPage {
+
+	public MealPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
+		super(driver, wait, js);
+	}
+
+	public WebElement getAddToCartButton() {
+		return this.driver.findElement(By.xpath("//*[@id=\"body\"]/section[1]/div/div/div[2]/div/div[3]/div[2]/a"));
+	}
+
+	public WebElement getFavoriteButton() {
+		return this.driver.findElement(By.xpath("//*[@id=\"item_116\"]"));
+
+	}
+
+	public WebElement getQuantity() {
+		return this.driver
+				.findElement(By.xpath("//*[@id=\"body\"]/section[1]/div/div/div[2]/div/div[3]/div[1]/ul/li[3]/input"));
+	}
+
+	public WebElement getClearAllButton() {
+		return this.driver.findElement(By.xpath("//*[@id=\"cartSummary\"]/div/div[1]/a[2]"));
+	}
+
+	public void addToFavorites() {
+		this.getFavoriteButton().click();
+	}
+
+	public void addMealToCart(int amount) {
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		this.getQuantity().sendKeys(Keys.CONTROL, "a");
+		this.getQuantity().sendKeys(String.valueOf(amount));
+		js.executeScript("arguments[0].click();", getAddToCartButton());
+
+	}
+
+	public void clearCart() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click();", this.getClearAllButton());
+
+	}
 }
--- src/tests/MealItemTest.java
+++ src/tests/MealItemTest.java
@@ -1,5 +1,116 @@
 package tests;
 
-public class MealItemTest {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import pages.LocationPopUpPage;
+import pages.LoginPage;
+import pages.MealPage;
+import pages.NotificationSystemPage;
+
+public class MealItemTest extends BasicTest {
+
+	public MealItemTest() {
+		super();
+	}
+
+	@Test(priority = 0)
+
+	public void AddMealToCartTest() throws InterruptedException {
+		LocationPopUpPage lpp = new LocationPopUpPage(driver, wait, js);
+		NotificationSystemPage nsp = new NotificationSystemPage(driver, wait, js);
+		MealPage mp = new MealPage(driver, wait, js);
+
+		this.driver.navigate().to(baseURL + "meal/chicken-sandwich-beef-empanadas-combo");
+		lpp.closePopUp();
+
+		mp.addMealToCart(4);
+
+		Assert.assertTrue(nsp.getNotificationMessage()
+				.contains("The Following Errors Occurred:" + "\n" + "Please Select Location"));
+		
+		nsp.waitForNotDisappearance();
+		
+		Thread.sleep(1000);
+		
+		lpp.OpenPopUp();
+		lpp.setLocation("City Center - Albany");
+
+		Thread.sleep(1000);
+
+		mp.addMealToCart(4);
+
+		Assert.assertTrue(nsp.getNotificationMessage().contains("Meal Added To Cart"), "[ERROR] Cart is empty!");
+
+		Thread.sleep(2000);
+	}
+
+	@Test(priority = 5)
+
+	public void AddMealToFavouriteTest() throws InterruptedException {
+		LocationPopUpPage lpp = new LocationPopUpPage(driver, wait, js);
+		NotificationSystemPage nsp = new NotificationSystemPage(driver, wait, js);
+		MealPage mp = new MealPage(driver, wait, js);
+		LoginPage lp = new LoginPage(driver, wait, js);
+
+		this.driver.navigate().to(baseURL + "meal/chicken-sandwich-beef-empanadas-combo");
+		lpp.closePopUp();
+
+		mp.addToFavorites();
+
+		Assert.assertTrue(nsp.getNotificationMessage().contains("Please login first!"), "ERROR: Already logged in!");
+		this.driver.navigate().to(baseURL + "guest-user/login-form");
+
+		lp.login(email, password);
+		this.driver.navigate().to(baseURL + "meal/chicken-sandwich-beef-empanadas-combo");
+		mp.addToFavorites();
+		Assert.assertTrue(nsp.getNotificationMessage().contains("Product has been added to your favorites."),
+				"ERROR: Product not added to favorites");
+	}
+
+	@Test(priority = 10)
+
+	public void ClearCartTest() throws InterruptedException, IOException {
+		LocationPopUpPage lpp = new LocationPopUpPage(driver, wait, js);
+		MealPage mp = new MealPage(driver, wait, js);
+		NotificationSystemPage nsp = new NotificationSystemPage(driver, wait, js);
+
+		this.driver.navigate().to(baseURL + "meals");
+
+		lpp.setLocation("City Center - Albany");
+
+		File file = new File("data/Data.xlsx");
+		FileInputStream fis = new FileInputStream(file);
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		XSSFSheet sheet = wb.getSheet("Meals");
+		SoftAssert sa = new SoftAssert();
+
+		for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+
+			XSSFRow row = sheet.getRow(i);
+			String meal = row.getCell(0).getStringCellValue();
+			this.driver.navigate().to(meal);
+			mp.addMealToCart(2);
+			sa.assertTrue(nsp.getNotificationMessage().contains("Meal Added To Cart"), "[ERROR] Cart is empty!");
+
+		}
+
+		Thread.sleep(2000);
+
+		mp.clearCart();
+		Assert.assertTrue(nsp.getNotificationMessage().contains("All meals removed from Cart successfully"),
+				"[ERROR] Cart is not cleared!");
+
+		fis.close();
+		wb.close();
+	}
 
 }
